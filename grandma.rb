27f556356f78090f72951f2938ddd93250402055
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-31 13:26:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,1 @@
-if grandma == >"40"
-  puts if_grandma_is_greater_than_yours
-  elsif == "grandma cooks better than yours"
-  # HUH?! SPEAK UP SONNY 
-  #=>SPEAK TO GRANDMA 
-else == "grandma does cook better"
-  print FACTS
-end 
 
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
